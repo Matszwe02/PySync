ade7b1821e436270d27b9a85d8a4d0acc01dfff2
Matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2022-12-12 14:31:14 +0100

    Directory operations
    Now directories are properly detected as created or removed, with a proper order

--- main.py
+++ main.py
@@ -4,8 +4,46 @@ from datetime import datetime
 import hashlib
 
 
-src_path = "D:/"
+src_path = "D:/test/"
 
+
+
+def get_contents(path, recursion=0):
+    contents = []
+    indent = " " * recursion
+
+    # Use os.scandir() to iterate over the contents of the directory
+    with os.scandir(path) as entries:
+        for entry in entries:
+            # Get the name and type of the entry
+            element = entry.name
+            is_dir = entry.is_dir()
+
+            # Construct the full path of the entry
+            work_path = os.path.join(path, element)
+
+            # If the entry is a directory, append its name to the contents
+            # and recursively get its contents
+            if is_dir:
+                contents.append(indent + "-" + element + " /")
+                try:
+                    contents.extend(get_contents(work_path, recursion + 1))
+                except PermissionError:
+                    pass
+
+            # If the entry is a file, get its last modification time
+            else:
+                # Use the stat() method to get the last modification time
+                # and size of the file in a single call
+                file_stat = os.stat(work_path)
+                file_time = file_stat.st_mtime
+                file_size = file_stat.st_size
+
+                prehash_str = bytearray((str(file_size) + "&" + str(file_time)).encode("utf-8"))
+                file_hash = str(hashlib.sha256(prehash_str).hexdigest())[-16:]
+                contents.append(indent + "-" + element + " " + file_hash)
+
+    return contents
 
 
 
@@ -39,48 +77,6 @@ def format_dir_tree(lines):
 
 
 
-def get_contents(path, recursion=0):
-    contents = []
-    indent = " " * recursion
-
-    # Use os.scandir() to iterate over the contents of the directory
-    with os.scandir(path) as entries:
-        for entry in entries:
-            # Get the name and type of the entry
-            element = entry.name
-            is_dir = entry.is_dir()
-
-            # Construct the full path of the entry
-            work_path = os.path.join(path, element)
-
-            # If the entry is a directory, append its name to the contents
-            # and recursively get its contents
-            if is_dir:
-                contents.append(indent + "-" + element + " /")
-                try:
-                    contents.extend(get_contents(work_path, recursion + 1))
-                except PermissionError:
-                    pass
-
-            # If the entry is a file, get its last modification time, size,
-            # and hash, and append them to the contents
-            else:
-                # Use the stat() method to get the last modification time
-                # and size of the file in a single call
-                file_stat = os.stat(work_path)
-                file_time = file_stat.st_mtime
-                file_size = file_stat.st_size
-
-                # Use a bytearray instead of a bytes object to create
-                # the prehash_str variable
-                prehash_str = bytearray((str(file_size) + "&" + str(file_time)).encode("utf-8"))
-                file_hash = str(hashlib.sha256(prehash_str).hexdigest())[-16:]
-                contents.append(indent + "-" + element + " " + file_hash)
-
-    return contents
-
-
-
 
 print(f'Getting contents... {datetime.now().strftime("%H:%M:%S")}')
 
@@ -109,7 +105,6 @@ print(f'Counting differences... {datetime.now().strftime("%H:%M:%S")}')
 
 
 
-#TODO: make dirs work
 
 only_left_side = left_side.difference(right_side)
 only_right_side = right_side.difference(left_side)
@@ -118,22 +113,30 @@ common_files = left_side.intersection(right_side)
 
 only_left_side_names = {}
 only_left_side_hashes = {}
+only_left_side_dirs = []
 for element in only_left_side:
-    only_left_side_names[element[:-17]] = element
-    if only_left_side_hashes.get(element[-16:]):
-        only_left_side_hashes[element[-16:]] = only_left_side_hashes[element[-16:]] + [element]
+    if element[-1] == '/':
+        only_left_side_dirs.append(element)
     else:
-        only_left_side_hashes[element[-16:]] = [element]
+        only_left_side_names[element[:-17]] = element
+        if only_left_side_hashes.get(element[-16:]):
+            only_left_side_hashes[element[-16:]] = only_left_side_hashes[element[-16:]] + [element]
+        else:
+            only_left_side_hashes[element[-16:]] = [element]
 
 
 only_right_side_names = {}
 only_right_side_hashes = {}
+only_right_side_dirs = []
 for element in only_right_side:
-    only_right_side_names[element[:-17]] = element
-    if only_right_side_hashes.get(element[-16:]):
-        only_right_side_hashes[element[-16:]] = only_right_side_hashes[element[-16:]] + [element]
+    if element[-1] == '/':
+        only_right_side_dirs.append(element)
     else:
-        only_right_side_hashes[element[-16:]] = [element]
+        only_right_side_names[element[:-17]] = element
+        if only_right_side_hashes.get(element[-16:]):
+            only_right_side_hashes[element[-16:]] = only_right_side_hashes[element[-16:]] + [element]
+        else:
+            only_right_side_hashes[element[-16:]] = [element]
 
 
 common_files_names = {}
@@ -197,32 +200,37 @@ for element in only_right_side_names:
     if element in only_left_side_names:
         changed.append(only_left_side_names[element])
 
+dirs_created = sorted(only_left_side_dirs)
+dirs_deleted = sorted(only_right_side_dirs, reverse=True)
 
 
-print(f'Created elements: {created.__len__()}')
-print(f'Deleted elements: {deleted.__len__()}')
-print(f'Changed elements: {changed.__len__()}')
-print(f'Moved elements: {moved.__len__()}')
-print(f'Copied elements: {copied.__len__()}')
+print(f'Created files: {created.__len__()}')
+print(f'Deleted files: {deleted.__len__()}')
+print(f'Changed files: {changed.__len__()}')
+print(f'Moved files: {moved.__len__()}')
+print(f'Copied files: {copied.__len__()}')
+print(f'Created directories: {dirs_created.__len__()}')
+print(f'Deleted directories: {dirs_deleted.__len__()}')
+
 
 
 with open("changes.txt", "w", encoding="utf-8") as changes_file:
     
     
-    for item in deleted:
-        changes_file.write(f'Deleted {item}\n')
-        
-    for item in created:
-        changes_file.write(f'Created {item}\n')
-    
+    for item in dirs_created:
+        changes_file.write(f'DirCreated {item}\n')
     for item in changed:
         changes_file.write(f'Changed {item}\n')
-    
+    for item in created:
+        changes_file.write(f'Created {item}\n')
     for item in moved:
         changes_file.write(f'Moved {item}\n')
-    
     for item in copied:
         changes_file.write(f'Copied {item}\n')
+    for item in deleted:
+        changes_file.write(f'Deleted {item}\n')
+    for item in dirs_deleted:
+        changes_file.write(f'DirRemoved {item}\n')
         
         
         
