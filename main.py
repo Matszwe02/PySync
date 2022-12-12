bc4da2509cbaf647751167a703e1099ce35a2dcf
Matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2022-12-12 16:44:20 +0100

    Changed list_files to function
    It can be called by the copying function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import hashlib
 
 
-src_path = "D:/test/"
+src_path = "D:/"
 
 
 
@@ -77,165 +77,138 @@ def format_dir_tree(lines):
 
 
 
-
-print(f'Getting contents... {datetime.now().strftime("%H:%M:%S")}')
-
-current_tree = get_contents(src_path)
-
-
-
-
-print(f'Reading file... {datetime.now().strftime("%H:%M:%S")}')
-
-with open("file_tree.txt", "r", encoding="utf-8") as file_tree:
-    previous_tree = file_tree.readlines()
-
-
+def list_files(src_path : str, tree_lines : list):
     
-    
-print(f'Formatting tree... {datetime.now().strftime("%H:%M:%S")}')
+    current_tree = get_contents(src_path)
+    previous_tree = tree_lines
 
-left_side = set(format_dir_tree(current_tree))
-right_side = set(format_dir_tree(previous_tree))
+    left_side = set(format_dir_tree(current_tree))
+    right_side = set(format_dir_tree(previous_tree))
 
+    only_left_side = left_side.difference(right_side)
+    only_right_side = right_side.difference(left_side)
+    common_files = left_side.intersection(right_side)
 
-
-print(f'Counting differences... {datetime.now().strftime("%H:%M:%S")}')
-
-
-
-
-
-only_left_side = left_side.difference(right_side)
-only_right_side = right_side.difference(left_side)
-common_files = left_side.intersection(right_side)
-
-
-only_left_side_names = {}
-only_left_side_hashes = {}
-only_left_side_dirs = []
-for element in only_left_side:
-    if element[-1] == '/':
-        only_left_side_dirs.append(element)
-    else:
-        only_left_side_names[element[:-17]] = element
-        if only_left_side_hashes.get(element[-16:]):
-            only_left_side_hashes[element[-16:]] = only_left_side_hashes[element[-16:]] + [element]
+    only_left_side_names = {}
+    only_left_side_hashes = {}
+    only_left_side_dirs = []
+    for element in only_left_side:
+        if element[-1] == '/':
+            only_left_side_dirs.append(element)
         else:
-            only_left_side_hashes[element[-16:]] = [element]
+            only_left_side_names[element[:-17]] = element
+            if only_left_side_hashes.get(element[-16:]):
+                only_left_side_hashes[element[-16:]] = only_left_side_hashes[element[-16:]] + [element]
+            else:
+                only_left_side_hashes[element[-16:]] = [element]
 
 
-only_right_side_names = {}
-only_right_side_hashes = {}
-only_right_side_dirs = []
-for element in only_right_side:
-    if element[-1] == '/':
-        only_right_side_dirs.append(element)
-    else:
-        only_right_side_names[element[:-17]] = element
-        if only_right_side_hashes.get(element[-16:]):
-            only_right_side_hashes[element[-16:]] = only_right_side_hashes[element[-16:]] + [element]
+    only_right_side_names = {}
+    only_right_side_hashes = {}
+    only_right_side_dirs = []
+    for element in only_right_side:
+        if element[-1] == '/':
+            only_right_side_dirs.append(element)
         else:
-            only_right_side_hashes[element[-16:]] = [element]
+            only_right_side_names[element[:-17]] = element
+            if only_right_side_hashes.get(element[-16:]):
+                only_right_side_hashes[element[-16:]] = only_right_side_hashes[element[-16:]] + [element]
+            else:
+                only_right_side_hashes[element[-16:]] = [element]
 
 
-common_files_names = {}
-common_files_hashes = {}
-for element in common_files:
-    common_files_names[element[:-17]] = element
-    if common_files_hashes.get(element[-16:]):
-        common_files_hashes[element[-16:]] = common_files_hashes[element[-16:]] + [element]
-    else:
-        common_files_hashes[element[-16:]] = [element]
+    common_files_names = {}
+    common_files_hashes = {}
+    for element in common_files:
+        common_files_names[element[:-17]] = element
+        if common_files_hashes.get(element[-16:]):
+            common_files_hashes[element[-16:]] = common_files_hashes[element[-16:]] + [element]
+        else:
+            common_files_hashes[element[-16:]] = [element]
 
 
-
-
-print(f'Summing up and saving differences... {datetime.now().strftime("%H:%M:%S")}')
-
-
-            
-
-local_create_files = []
-local_delete_files = []
-copied = []
-moved = []
-for element in only_left_side_hashes:
-    left_elements = only_left_side_hashes.get(element)
-    right_elements = only_right_side_hashes.get(element)
-    common_elements = common_files_hashes.get(element)
-    
-    for i, item in enumerate(left_elements):
-        if(left_elements.__len__() > i):
-            if right_elements and right_elements.__len__() > i:
-                moved.append(f'{right_elements[i]} >> {item}')
-                local_create_files.append(item)
-                local_delete_files.append(right_elements[i])
-            
-            elif common_elements:
-                copied.append(f'{common_elements[0]} >> {item}')
-                local_create_files.append(item)
-            
-print(f'Analyzed copies. Filling everything else... {datetime.now().strftime("%H:%M:%S")}')
-
-
-created = []
-for element in only_left_side_names:
-    if not element in only_right_side_names:
-        created.append(only_left_side_names[element])
-
-created = list(set(created).difference(set(local_create_files)))
-
-
-deleted = []
-for element in only_right_side_names:
-    if not element in only_left_side_names:
-        deleted.append(only_right_side_names[element])
+    local_create_files = []
+    local_delete_files = []
+    copied = []
+    moved = []
+    for element in only_left_side_hashes:
+        left_elements = only_left_side_hashes.get(element)
+        right_elements = only_right_side_hashes.get(element)
+        common_elements = common_files_hashes.get(element)
         
-deleted = list(set(deleted).difference(set(local_delete_files)))
+        for i, item in enumerate(left_elements):
+            if(left_elements.__len__() > i):
+                if right_elements and right_elements.__len__() > i:
+                    moved.append(f'{right_elements[i]} >> {item}')
+                    local_create_files.append(item)
+                    local_delete_files.append(right_elements[i])
+                
+                elif common_elements:
+                    copied.append(f'{common_elements[0]} >> {item}')
+                    local_create_files.append(item)
+                
 
+    created = []
+    for element in only_left_side_names:
+        if not element in only_right_side_names:
+            created.append(only_left_side_names[element])
 
-changed = []
-for element in only_right_side_names:
-    if element in only_left_side_names:
-        changed.append(only_left_side_names[element])
+    created = list(set(created).difference(set(local_create_files)))
 
-dirs_created = sorted(only_left_side_dirs)
-dirs_deleted = sorted(only_right_side_dirs, reverse=True)
+    deleted = []
+    for element in only_right_side_names:
+        if not element in only_left_side_names:
+            deleted.append(only_right_side_names[element])
+            
+    deleted = list(set(deleted).difference(set(local_delete_files)))
 
+    changed = []
+    for element in only_right_side_names:
+        if element in only_left_side_names:
+            changed.append(only_left_side_names[element])
 
-print(f'Created files: {created.__len__()}')
-print(f'Deleted files: {deleted.__len__()}')
-print(f'Changed files: {changed.__len__()}')
-print(f'Moved files: {moved.__len__()}')
-print(f'Copied files: {copied.__len__()}')
-print(f'Created directories: {dirs_created.__len__()}')
-print(f'Deleted directories: {dirs_deleted.__len__()}')
+    dirs_created = sorted(only_left_side_dirs)
+    dirs_deleted = sorted(only_right_side_dirs, reverse=True)
+    return {"DirCreated" : dirs_created, "Changed" : changed, "Created" : created, "Moved" : moved, "Copied" : copied, "Deleted" : deleted, "DirDeleted" : dirs_deleted, "TreeInstance" : current_tree}
 
+lines = []
+with open("file_tree.txt", "r", encoding="utf-8") as file_tree:
+    lines = file_tree.readlines()
+
+x = list_files(src_path, lines)
 
 
 with open("changes.txt", "w", encoding="utf-8") as changes_file:
     
     
-    for item in dirs_created:
-        changes_file.write(f'DirCreated {item}\n')
-    for item in changed:
-        changes_file.write(f'Changed {item}\n')
-    for item in created:
-        changes_file.write(f'Created {item}\n')
-    for item in moved:
-        changes_file.write(f'Moved {item}\n')
-    for item in copied:
-        changes_file.write(f'Copied {item}\n')
-    for item in deleted:
-        changes_file.write(f'Deleted {item}\n')
-    for item in dirs_deleted:
-        changes_file.write(f'DirRemoved {item}\n')
+    for item in x["DirCreated"]:
+        changes_file.write(f'DirCreated : {item}\n')
+    for item in x["Changed"]:
+        changes_file.write(f'  Changed  : {item}\n')
+    for item in x["Created"]:
+        changes_file.write(f'  Created  : {item}\n')
+    for item in x["Moved"]:
+        changes_file.write(f'   Moved   : {item}\n')
+    for item in x["Copied"]:
+        changes_file.write(f'  Copied   : {item}\n')
+    for item in x["Deleted"]:
+        changes_file.write(f'  Deleted  : {item}\n')
+    for item in x["DirDeleted"]:
+        changes_file.write(f'DirRemoved : {item}\n')
         
         
         
 print(f'Done! {datetime.now().strftime("%H:%M:%S")}')
 
 
+# print(f'Created files: {created.__len__()}')
+# print(f'Deleted files: {deleted.__len__()}')
+# print(f'Changed files: {changed.__len__()}')
+# print(f'Moved files: {moved.__len__()}')
+# print(f'Copied files: {copied.__len__()}')
+# print(f'Created directories: {dirs_created.__len__()}')
+# print(f'Deleted directories: {dirs_deleted.__len__()}')
+
+
 with open("file_tree.txt", "w", encoding="utf-8") as file_tree:
-    file_tree.write('\n'.join(current_tree))
+    file_tree.write('\n'.join(x["TreeInstance"]))
