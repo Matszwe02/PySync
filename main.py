af2f71e677e498227e998af7120fd4a0c1628306
Matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2022-12-29 12:26:51 +0100

    Added multithreading and better ui

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import queue
 import shutil
 import time
 from datetime import datetime
@@ -8,10 +9,12 @@ import runpy
 import json
 try:
     from tqdm import tqdm
+    import msvcrt
 except:
     pass
 import atexit
 import copy
+import concurrent.futures
 
 
 
@@ -34,10 +37,61 @@ forbidden_paths = config["ForbiddenPaths"]
 allowed_paths = config["AllowedPaths"]
 file_tree_name = config["FileTreeName"]
 last_list_threshold = config["LastListThreshold"]
+large_file_size = config["LargeFileSize"]
+small_file_threads = config["SmallFileThreads"]
+
+task_queue = queue.Queue()
+errors = 0
 
 if not os.path.exists(file_tree_name):
     open(file_tree_name, 'a').close()
+    
+    
+def prRed(skk): print("\033[91m{}\033[00m".format(skk))
+def prGreen(skk): print("\033[92m{}\033[00m".format(skk))
+def prYellow(skk): print("\033[93m{}\033[00m".format(skk))
+def prBlue(skk): print("\033[94m{}\033[00m".format(skk))
+def prPurple(skk): print("\033[95m{}\033[00m".format(skk))
+def prCyan(skk): print("\033[96m{}\033[00m".format(skk))
+def prLightGray(skk): print("\033[97m{}\033[00m".format(skk))
+def prBlack(skk): print("\033[98m{}\033[00m".format(skk))
 
+
+def file_action(action_info):
+    global errors
+    item = action_info["item"]
+    src_path = action_info["src"]
+    dst_path = action_info["dst"]
+    tq = action_info["tqdmInstance"]
+    action = action_info["Action"]
+    changes = action_info["Changes"]
+    try:
+        if action == "Copy":
+            item1 = remove_file_hash(item)
+            shutil.copy2(src_path + item1, dst_path + item1)
+            tq.update()
+        
+        if action == "Delete":
+            item1 = remove_file_hash(item)
+            os.remove(dst_path + item1)
+            tq.update()
+            
+        changes.remove(item)
+    except Exception as e:
+        prRed(f'\r{" " * 200}\r ERROR {type(e).__name__}  With file : {src_path + item}')
+        errors += 1
+        time.sleep(0.5)
+        tq.update()
+
+
+def worker():
+  while True:
+    work = task_queue.get()
+    if work is None:
+      # Sentinel value reached, break the loop
+      break
+    file_action(work)
+    task_queue.task_done()
 
 
 def update_nas_tree():
@@ -53,7 +107,7 @@ def update_nas_tree():
     print("NAS filetree download complete")
 
 
-def get_contents(path, recursion=0):
+def get_contents(path, local_path = "", recursion=0):
     contents = []
     indent = " " * recursion
 
@@ -66,14 +120,16 @@ def get_contents(path, recursion=0):
 
             # Construct the full path of the entry
             work_path = os.path.join(path, element)
-
+            local_path_1 = os.path.join(local_path, element).replace("\\", "/")
             # If the entry is a directory, append its name to the contents
             # and recursively get its contents
             if is_dir:
                 if (recursion > 0 or ((not element in forbidden_paths) and ((element + '/') in allowed_paths or '*' in allowed_paths))):
+                    if local_path_1 + '/' in forbidden_paths:
+                        continue
                     contents.append(indent + "-" + element + " /")
                     try:
-                        contents.extend(get_contents(work_path, recursion + 1))
+                        contents.extend(get_contents(work_path, local_path_1, recursion + 1))
                     except PermissionError:
                         print("PERMISSION ERROR WHILE CREATING " + element)
 
@@ -84,9 +140,12 @@ def get_contents(path, recursion=0):
                 file_stat = os.stat(work_path)
                 file_time = file_stat.st_mtime
                 file_size = file_stat.st_size
+                
+                size = str(hex(str(file_size).__len__()))[-1]
+                
 
                 prehash_str = bytearray((str(file_size) + "&" + str(file_time)).encode("utf-8"))
-                file_hash = str(hashlib.sha256(prehash_str).hexdigest())[-16:]
+                file_hash = (str(hashlib.sha256(prehash_str).hexdigest()) + size)[-16:]
                 contents.append(indent + "-" + element + " " + file_hash)
 
     return contents
@@ -232,7 +291,7 @@ def exit_save_changes():
     global to_upload, to_download
     save_changes(to_upload, "upload.json")
     save_changes(to_download, "download.json")
-    print("Saved session changes")
+    prGreen("Saved session changes")
 
 
 def get_nas_tree():
@@ -268,81 +327,149 @@ def split_move_copy(command: str):
 
 
 def file_operation(changes : dict, from_path: str, to_path: str):
+    global errors
     
-    changes_save = copy.deepcopy(changes)
+    global large_file_size
+    changes_1 = copy.deepcopy(changes)
     
-    for item in tqdm(sorted(changes_save["DirCreated"]), "DirCreated"):
+    for item in tqdm(sorted(changes_1["DirCreated"]), "DirCreated", colour = "CYAN"):
         try:
             os.mkdir(to_path + item)
-            changes["DirCreated"].remove(item)
         except FileExistsError:
-            print(f"FILE EXISTS", end=' ')
+            prYellow(f"FILE EXISTS")
             print(to_path + item)
-            changes["DirCreated"].remove(item)
-            pass
         except FileNotFoundError:
-            print(f"FILE NOT FOUND CRITICAL", end=' ')
+            prRed(f"FILE NOT FOUND CRITICAL")
             print(to_path + item)
-            changes["DirCreated"].remove(item)
+        changes["DirCreated"].remove(item)
     
     
-    for item in tqdm(changes_save["Created"], "Created"):
+    
+    small_files = []
+    large_files = []
+    for item in changes_1["Created"]:
+        if int(item[-1], 16) >= large_file_size:
+            large_files.append(item)
+        else:
+            small_files.append(item)
+    
+    with tqdm(total = small_files.__len__(), desc="Created SmallFiles", colour = "CYAN") as tq:
+        for item in small_files:
+            task_queue.put({"Action" : "Copy", "src" : from_path, "dst" : to_path, "item" : item, "tqdmInstance" : tq, "Changes" : changes["Created"]})
+        
+        task_queue.join()
+    
+    for item in tqdm(large_files, "Created LargeFiles", colour = "CYAN"):
         item1 = remove_file_hash(item)
         try:
             shutil.copy2(from_path + item1, to_path + item1)
             changes["Created"].remove(item)
-        except PermissionError:
-            print(" PERMISSION ERROR WITH FILE " + item1)
+        except Exception as e:
+            prRed(f'\r{" " * 200}\r ERROR {type(e).__name__}  With file : {from_path + item}')
+            time.sleep(0.5)
+            errors += 1
+    
+    
+    small_files = []
+    large_files = []
+    for item in changes_1["Changed"]:
+        if int(item[-1], 16) >= large_file_size:
+            large_files.append(item)
+        else:
+            small_files.append(item)
+    
+    with tqdm(total = small_files.__len__(), desc="Changed SmallFiles", colour = "CYAN") as tq:
+        for item in small_files:
+            task_queue.put({"Action" : "Copy", "src" : from_path, "dst" : to_path, "item" : item, "tqdmInstance" : tq, "Changes" : changes["Changed"]})
+        
+        task_queue.join()
                 
-    for item in tqdm(changes_save["Changed"], "Changed"):
-        item1 = remove_file_hash(item)
-        shutil.copy2(from_path + item1, to_path + item1)
-        changes["Changed"].remove(item)
-    
-    for item in tqdm(changes_save["Moved"], "Moved"):
-        files = split_move_copy(item)
-        shutil.move(to_path + files[0], to_path + files[1]) # FIXME: 
-        changes["Moved"].remove(item)
-    
-    for item in tqdm(changes_save["Copied"], "Copied"):
-        files = split_move_copy(item)
-        shutil.copy2(to_path + files[0], to_path + files[1])
-        changes["Copied"].remove(item)
-    
-    for item in tqdm(changes_save["Deleted"], "Deleted"):
+    for item in tqdm(large_files, "Changed LargeFiles", colour = "CYAN"):
         item1 = remove_file_hash(item)
         try:
-            os.remove(to_path + item1)
-            changes["Deleted"].remove(item)
-        except PermissionError:
-            pass
+            shutil.copy2(from_path + item1, to_path + item1)
+            changes["Changed"].remove(item)
+        except Exception as e:
+            prRed(f'\r{" " * 200}\r ERROR {type(e).__name__}  With file : {from_path + item}')
+            time.sleep(0.5)
+            errors += 1
     
-    for item in tqdm(sorted(changes_save["DirDeleted"], reverse=True), "DirDeleted"):
+    
+    for item in tqdm(changes_1["Moved"], "Moved", colour = "CYAN"):
+        files = split_move_copy(item)
+        try:
+            shutil.move(to_path + files[0], to_path + files[1]) # FIXME: 
+            changes["Moved"].remove(item)
+        except Exception as e:
+            prRed(f'\r{" " * 200}\r ERROR {type(e).__name__}  With file : {from_path + item}')
+            time.sleep(0.5)
+            errors += 1
+    
+    for item in tqdm(changes_1["Copied"], "Copied", colour = "CYAN"):
+        files = split_move_copy(item)
+        try:
+            shutil.copy2(to_path + files[0], to_path + files[1])
+            changes["Copied"].remove(item)
+        except Exception as e:
+            prRed(f'\r{" " * 200}\r ERROR {type(e).__name__}  With file : {from_path + item}')
+            time.sleep(0.5)
+            errors += 1
+            
+    
+    
+    with tqdm(total = changes_1["Deleted"].__len__(), desc="Deleted", colour = "CYAN") as tq:
+        for item in changes_1["Deleted"]:
+            task_queue.put({"Action" : "Delete", "src" : from_path, "dst" : to_path, "item" : item, "tqdmInstance" : tq, "Changes" : changes["Deleted"]})
+        task_queue.join()
+    
+    # for item in tqdm(changes["Deleted"], "Deleted", colour = "CYAN"):
+    #     item1 = remove_file_hash(item)
+    #     try:
+    #         os.remove(to_path + item1)
+    #     except Exception as e 
+    # prRed(f'\r{" " * 200}\r ERROR {type(e).__name__}  With file : {from_path + item}')PermissionError:
+    #         pass
+    
+    for item in tqdm(sorted(changes_1["DirDeleted"], reverse=True), "DirDeleted", colour = "CYAN"):
         # item = remove_file_hash(item)
         try:
             os.rmdir(to_path + item)
             changes["DirDeleted"].remove(item)
-        except:
-            pass
+        except Exception as e:
+            prRed(f'\r{" " * 200}\r ERROR {type(e).__name__}  With file : {from_path + item}')
+            time.sleep(0.5)
+            errors += 1
+
+
 
 
 if __name__ == "__main__" and mode == "PC":
 
     print(f'start! {datetime.now().strftime("%H:%M:%S")}')
     resume = False
-    # try:
-    #     to_download = load_changes("download.json")
-    #     to_upload = load_changes("upload.json")
-    # except:
-    #     to_download = {}
-    #     to_upload = {}
-    # for key in to_download.keys():
-    #     if to_download[key].__len__() > 0:
-    #         resume = True
-    # for key in to_upload.keys():
-    #     if to_upload[key].__len__() > 0:
-    #         resume = True
-                
+    
+    for _ in range(small_file_threads):
+        t = threading.Thread(target=worker)
+        t.daemon = True
+        t.start()
+    
+    
+    try:
+        to_download = load_changes("download.json")
+        to_upload = load_changes("upload.json")
+    except:
+        to_download = {}
+        to_upload = {}
+    for key in to_download.keys():
+        if to_download[key].__len__() > 0:
+            resume = True
+    for key in to_upload.keys():
+        if to_upload[key].__len__() > 0:
+            resume = True
+    
+    tu_len = 0
+    td_len = 0
+    
     if not resume:
         
         print("Not resuming")
@@ -358,28 +485,74 @@ if __name__ == "__main__" and mode == "PC":
 
 
         # to_download = list_changes(right_tree, left_tree)
-        to_download = list_changes(right_tree, current_files_tree)
         to_upload = list_changes(current_files_tree, left_tree)
+            
+        to_download = list_changes(right_tree, left_tree)
     
     else:
-        print("resuming actions...")
-    # print(current_files_tree)
-    # print(left_tree)
-    # print(right_tree)
+        print("resuming previous sync")
+        
+    
+    
+    for key in to_upload:
+        tu_len += to_upload[key].__len__()
+    for key in to_download:
+        td_len += to_download[key].__len__()
+
 
     save_changes(to_upload, "upload.json")
     save_changes(to_download, "download.json")
     
     atexit.register(exit_save_changes)
     
+    prCyan(f'There will be {tu_len} upload changes and {td_len} download changes.\nYou can check them in upload.json and download.json.')
+    print("Press \t 't' for file_tree update \t 'r' to replace file tree with nas \t 'q' to quit \t 'c' to clear sync queue \t anything else to sync\n")
+    action = str(msvcrt.getch())[2]
+    print(action)
+    
+    if action in ['t', 'r', 'c', 'q']:
+        if action == 't':
+            update_local_tree(current_files_tree)
+            prPurple("SYNC OVERRIDE \t file_tree update")
+        if action == 'r':
+            update_local_tree(nas_contents)
+            prPurple("SYNC OVERRIDE \t replace file tree with nas")
+        if action == 'c':
+            save_changes({}, "upload.json")
+            save_changes({}, "download.json")
+            prPurple("SYNC OVERRIDE \t clear sync queue")
+        if action == 'q':
+            pass
+        atexit.unregister(exit_save_changes)
+        exit()
+    
+    os.system("cls")
+    no_errors = True
+    
     if config["DownloadOnly"] == False:
-        print("Uploading files...")
+        prBlue("\n   __________  Uploading files  __________")
+        errors = 0
         file_operation(to_upload, src_path, nas_path)
+        if errors > 0:
+            prYellow("Some errors occured. Retrying operations...")
+            file_operation(to_upload, src_path, nas_path)
+        if errors > 0:
+            prRed("Upload completed with errors. Check upload.json for more details.")
+            no_errors = False
     else:
         print("skipping uploading files")
+        
     
-    print("Downloading files...")
+    prBlue("\n   _________  Downloading files  _________")
+    errors = 0
     file_operation(to_download, nas_path, src_path)
+    if errors > 0:
+        prYellow("Some errors occured. Retrying operations...")
+        errors = 0
+        file_operation(to_download, nas_path, src_path)
+    if errors > 0:
+        prRed("Download completed with errors. Check download.json for more details.")
+        no_errors = False
     
     # except FileNotFoundError:
     #     print("ERROR during sync")
@@ -390,10 +563,13 @@ if __name__ == "__main__" and mode == "PC":
         
     # atexit.unregister(exit_save_changes)
     
-    current_files_tree = get_contents(src_path)     
-    update_local_tree(current_files_tree)
+    print(f'\nDone!')
+    
+    if no_errors:
+        update_local_tree(get_contents(src_path))
+    else:
+        prYellow("Syncing incomplete. Cannot run next sync before completing this one.")
             
-    print(f'Done! {datetime.now().strftime("%H:%M:%S")}')
 
 
 if __name__ == "__main__" and mode == "NAS":
